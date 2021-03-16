63ecff8f4871bb6400e846808361ab566255e16b
taiki_hamaguchi <taiki_hamaguchi@jrass.jp>
2021-03-17 02:14:18 +0900

    要件を満たしました

---
+++ SecondKadaiApp/PrintViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PrintViewController.swift
+//  SecondKadaiApp
+//
+//  Created by PC-SYSKAI553 on 2021/03/17.
+//
+
+import UIKit
+
+class PrintViewController: UIViewController {
+    
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    var name: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        if name.isEmpty {
+            NameLabel.text = "空白です"
+        } else {
+            NameLabel.text = "こんにちは、" + name + "さん"
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var InputName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToPrint" {
+            let PVC: PrintViewController = segue.destination as! PrintViewController
+            PVC.name = InputName.text
+        }
+    }
+    //PVCから戻った時に呼ばれる
+    @IBAction func back(_ segue: UIStoryboardSegue) {
     }
 
 
